819db48da68d4cd5828cf9d8bae1860c9dba0bc7
chiconcota <lytatthanh@gmail.com>
2026-01-30 04:04:37 +0700

    Fix critical dashboard crash and restore editor icon

--- dynamic-tags-lite.php
+++ dynamic-tags-lite.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Dynamic Tags Lite
  * Description: Lightweight dynamic tags for Gutenberg.
- * Version: 1.7.0
+ * Version: 1.7.1
  * Author: Antigravity
  * Text Domain: dynamic-tags-lite
  */
